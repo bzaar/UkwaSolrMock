c3149a5a02efee854d0e156086b3fe6fa2d328f3
Sergey Slepov <slepov@gmail.com>
2017-06-11 17:06:33 +0100

    Add project files.

---
+++ UkwaSolrMock/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UkwaSolrMock.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ UkwaSolrMock/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UkwaSolrMock.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
